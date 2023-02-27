66fe987161f932d5e3fe4706d975633106083551
Genevieve Ferrari <genferrari@gmail.com>
2023-02-27 12:48:37 -0800

    finished comment and index models

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require("./User");
+const Comment = require("./Comment");
+const Post = require("./Post");
+
+// Feeling.belongsToMany(User);
+// User.hasMany
+
+// relationship between User and Post
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "post_id",
+});
+
+// relationship between User and comment
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+//relationship between comment and post
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Comment, Post };
