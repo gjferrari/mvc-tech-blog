73cdb5b6ebf33828f654c63250bd06bbedcba954
Genevieve Ferrari <genferrari@gmail.com>
2023-02-27 14:33:48 -0800

    created seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,41 +1,45 @@
-const { User, Feeling } = require("../models");
-const seedCategories = require("./category-seeds");
-const seedProducts = require("./product-seeds");
-const seedTime = require("./time-seeds");
-const seedProductTimes = require("./product-time-seeds");
-const seedQuestions = require("./question-seed");
-const userData = require("./userData.json");
-const feelingData = require("./feelingData.json");
-
 const sequelize = require("../config/connection");
+
+const { User, Post, Comment } = require("../models");
+
+const userData = require("./userData.JSON");
+const postData = require("./postData.JSON");
+const commentData = require("./commentData.JSON");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log("\n----- DATABASE SYNCED -----\n");
 
-  await seedTime();
-  console.log("\n----- TIME SEEDED -----\n");
+  // await seedTime();
+  // console.log("\n----- TIME SEEDED -----\n");
 
-  await seedCategories();
-  console.log("\n----- CATEGORIES SEEDED -----\n");
+  // await seedCategories();
+  // console.log("\n----- CATEGORIES SEEDED -----\n");
 
-  await seedProducts();
-  console.log("\n----- PRODUCTS SEEDED -----\n");
+  // await seedProducts();
+  // console.log("\n----- PRODUCTS SEEDED -----\n");
 
-  await seedProductTimes();
-  console.log("\n----- PRODUCT TIME SEEDED -----\n");
+  // await seedProductTimes();
+  // console.log("\n----- PRODUCT TIME SEEDED -----\n");
 
-  await seedQuestions();
-  console.log("\n----- PRODUCT QUESTIONS SEEDED -----\n");
+  // await seedQuestions();
+  // console.log("\n----- PRODUCT QUESTIONS SEEDED -----\n");
 
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const feeling of feelingData) {
-    await Feeling.create({
-      ...feeling,
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
