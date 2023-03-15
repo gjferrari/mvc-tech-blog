a9865420173ca3f74560a326e04919ab988743df
Genevieve Ferrari <genferrari@gmail.com>
2023-03-15 13:47:53 -0700

    corrected seed.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -3,9 +3,23 @@ const sequelize = require("../config/connection");
 const { User, Post, Comment } = require("../models");
 
 const userData = require("./userData.JSON");
-// const postData = require("./postData.json");
-// const commentData = require("./commentData.JSON");
+const postData = require("./postData.JSON");
+const commentData = require("./commentData.JSON");
 
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const post = await Post.bulkCreate(postData);
+
+  const comment = await Comment.bulkCreate(commentData);
+
+  process.exit(0);
+};
 // const commentData = [
 //   {
 //     content: "Me too! Can't get enough of it!",
@@ -17,52 +31,52 @@ const userData = require("./userData.JSON");
 //   },
 // ];
 
-console.log("Hello", commentData);
 
-const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log("\n----- DATABASE SYNCED -----\n");
 
-  // await seedTime();
-  // console.log("\n----- TIME SEEDED -----\n");
+// const seedAll = async () => {
+//   await sequelize.sync({ force: true });
+//   console.log("\n----- DATABASE SYNCED -----\n");
 
-  // await seedCategories();
-  // console.log("\n----- CATEGORIES SEEDED -----\n");
+//   // await seedTime();
+//   // console.log("\n----- TIME SEEDED -----\n");
 
-  // await seedProducts();
-  // console.log("\n----- PRODUCTS SEEDED -----\n");
+//   // await seedCategories();
+//   // console.log("\n----- CATEGORIES SEEDED -----\n");
 
-  // await seedProductTimes();
-  // console.log("\n----- PRODUCT TIME SEEDED -----\n");
+//   // await seedProducts();
+//   // console.log("\n----- PRODUCTS SEEDED -----\n");
 
-  // await seedQuestions();
-  // console.log("\n----- PRODUCT QUESTIONS SEEDED -----\n");
+//   // await seedProductTimes();
+//   // console.log("\n----- PRODUCT TIME SEEDED -----\n");
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+//   // await seedQuestions();
+//   // console.log("\n----- PRODUCT QUESTIONS SEEDED -----\n");
 
-  // for (const post of postData) {
-  //   await Post.create({
-  //     ...post,
-  //     user_id: users[Math.floor(Math.random() * users.length)].id,
-  //   });
-  // }
+//   const users = await User.bulkCreate(userData, {
+//     individualHooks: true,
+//     returning: true,
+//   });
 
-  for (const comment of commentData) {
-    await Comment.create({
-      ...comment,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+//   // for (const post of postData) {
+//   //   await Post.create({
+//   //     ...post,
+//   //     user_id: users[Math.floor(Math.random() * users.length)].id,
+//   //   });
+//   // }
 
-  // await seedUser();
-  // console.log("\n----- USERS SEEDED -----\n");
+//   // for (const comment of commentData) {
+//   //   await Comment.create({
+//   //     ...comment,
+//   //     user_id: users[Math.floor(Math.random() * users.length)].id,
+//   //   });
+//   // }
 
-  // console.log("\n-----USERS SEEDED-----\n");
+//   // await seedUser();
+//   // console.log("\n----- USERS SEEDED -----\n");
 
-  process.exit(0);
-};
+//   // console.log("\n-----USERS SEEDED-----\n");
+
+//   process.exit(0);
+// };
 
 seedAll();
