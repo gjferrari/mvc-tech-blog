ebe7fb2f35ac634cd4622797ab93c7914e889117
Genevieve Ferrari <genferrari@gmail.com>
2023-03-18 15:12:36 -0700

    changed post to edit in router.get function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -31,7 +31,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/post/:id", async (req, res) => {
+router.get("/edit/:id", async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [
@@ -39,12 +39,15 @@ router.get("/post/:id", async (req, res) => {
           model: User,
           attributes: ["name"],
         },
+        {
+          model: Comment,
+        },
       ],
     });
 
     const posts = postData.get({ plain: true });
 
-    res.render("singlePost", {
+    res.render("edit-post", {
       posts,
       logged_in: req.session.logged_in,
     });
@@ -52,35 +55,5 @@ router.get("/post/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-// // Use withAuth middleware to prevent access to route
-// router.get("/profile", withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ["password"] },
-//       include: [{ model: Project }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render("profile", {
-//       ...user,
-//       logged_in: true,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get("/login", (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (req.session.logged_in) {
-//     res.redirect("/profile");
-//     return;
-//   }
-
-//   res.render("login");
-// });
 
 module.exports = router;
