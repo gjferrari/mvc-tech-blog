434707900c5ab8951aad6b68c4e3e64c9d2efb08
Genevieve Ferrari <genferrari@gmail.com>
2023-03-19 11:11:41 -0700

    building out routes

---
+++ controllers/api/dashboardRoutes.js
@@ -0,0 +1,76 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "content", "title", "date_created"],
+    include: [
+      {
+        model: User,
+        attributes: ["name"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "content", "post_id", "user_id", "date_created"],
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+    ],
+  })
+    .then((postData) => {
+      const posts = postData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", {
+        posts,
+        loggedIn: true,
+        name: req.session.name,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: ["id", "content", "title", "date_created"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "content", "post_id", "user_id", "date_created"],
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["name"],
+      },
+    ],
+  })
+    .then((postData) => {
+      if (postData) {
+        const post = postData.get({ plain: true });
+
+        res.render("edit-post", {
+          post,
+          loggedIn: true,
+          username: req.session.name,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
